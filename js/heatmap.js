44d057fe2cfbed7acd562f72111266262e4810c0
Ludovic Launer <l.launer@idosens.com>
2020-02-17 09:41:46 +0100

    Add possibility to load multiple flights

--- js/heatmap.js
+++ js/heatmap.js
@@ -1,63 +1,143 @@
-﻿toastr.options = {
-    "closeButton": false,
-    "debug": false,
-    "newestOnTop": false,
-    "progressBar": true,
-    "positionClass": "toast-top-right",
-    "preventDuplicates": false,
-    "onclick": null,
-    "showDuration": "300",
-    "hideDuration": "1000",
-    "timeOut": "2000",
-    "extendedTimeOut": "1000",
-    "showEasing": "swing",
-    "hideEasing": "linear",
-    "showMethod": "fadeIn",
-    "hideMethod": "fadeOut"
+﻿mapboxgl.accessToken = 'pk.eyJ1IjoibHVkb3ZpY2xhdW5lciIsImEiOiJjazVtNzdvYzkwdTMzM2txa2xvaXJ0ZnY3In0.G6nft9RJo94MHqOOmIaYwA';
+
+var map = new mapboxgl.Map({
+    container: 'map',           // container id
+    style: "styles/style.json"
+});
+
+function filterBy(vario, altitude) {
+    // Add updated filters
+    var filters = ['all',
+        ['>=', 'vario', vario],
+        ['>=', 'alt_in', altitude]
+    ];
+
+    map.setFilter('layer-heatmap', filters);
+    map.setFilter('layer-vario-value', filters);
+
+    // Set the labels
+    $('#vario-label').html(vario);
+    $('#label-altitude-in').html(altitude);
 }
 
+map.on('load',
+    function () {
 
-function hexToHSL(H) {
-    // Convert hex to RGB first
-    let r = 0, g = 0, b = 0;
-    if (H.length == 4) {
-        r = "0x" + H[1] + H[1];
-        g = "0x" + H[2] + H[2];
-        b = "0x" + H[3] + H[3];
-    } else if (H.length == 7) {
-        r = "0x" + H[1] + H[2];
-        g = "0x" + H[3] + H[4];
-        b = "0x" + H[5] + H[6];
-    }
-    // Then to HSL
-    r /= 255;
-    g /= 255;
-    b /= 255;
-    let cmin = Math.min(r, g, b),
-        cmax = Math.max(r, g, b),
-        delta = cmax - cmin,
-        h = 0,
-        s = 0,
-        l = 0;
+        // --- Add data source ---
+        map.addSource('latest-data', {
+            type: 'geojson',
+            data: geojsonTargetDataSourceUrl
+        });
 
-    if (delta == 0)
-        h = 0;
-    else if (cmax == r)
-        h = ((g - b) / delta) % 6;
-    else if (cmax == g)
-        h = (b - r) / delta + 2;
-    else
-        h = (r - g) / delta + 4;
 
-    h = Math.round(h * 60);
+        // --- Layer: Heatmap
+        map.addLayer(
+            {
+                "id": "layer-heatmap",
+                "type": "heatmap",
+                "metadata": { "mapbox:group": "e686e2df54587f748da0baac43613eb5" },
+                "source": "latest-data",
+                "layout": {
+                },
+                "paint": {
+                    "heatmap-intensity": [
+                        "interpolate",
+                        ["linear"],
+                        ["zoom"],
+                        0,
+                        5,
+                        5,
+                        1.3
+                    ],
+                    "heatmap-radius": [
+                        "interpolate",
+                        ["linear"],
+                        ["zoom"],
+                        1,
+                        1,
+                        5,
+                        4,
+                        7,
+                        5,
+                        9,
+                        6,
+                        12,
+                        11,
+                        15,
+                        25
+                    ],
+                    "heatmap-color": [
+                        "interpolate",
+                        ["linear"],
+                        ["heatmap-density"],
+                        0,
+                        "rgba(0, 0, 255, 0)",
+                        0.1,
+                        "royalblue",
+                        0.3,
+                        "cyan",
+                        0.5,
+                        "lime",
+                        0.7,
+                        "yellow",
+                        1,
+                        "red"
+                    ],
+                    "heatmap-weight": [
+                        "case",
+                        [
+                            "<",
+                            ["get", "alt_in"],
+                            0
+                        ],
+                        0,
+                        [">", ["get", "vario"], 0],
+                        1,
+                        0
+                    ]
+                }
+            }
 
-    if (h < 0)
-        h += 360;
+        );
+        // End : Heatmap layer
 
-    l = (cmax + cmin) / 2;
-    s = delta == 0 ? 0 : delta / (1 - Math.abs(2 * l - 1));
-    s = +(s * 100).toFixed(1);
-    l = +(l * 100).toFixed(1);
+        // --- Layer: Vario values
+        map.addLayer(
+            {
+                "id": "layer-vario-value",
+                "type": "symbol",
+                "metadata": { "mapbox:group": "e686e2df54587f748da0baac43613eb5" },
+                "source": "latest-data",
+                "minzoom": 6,
+                "filter": [">", ["get", "vario"], 1],
+                "layout": {
+                    "text-justify": "auto",
+                    "text-size": 13,
+                    "text-offset": [0, 1],
+                    "text-field": [
+                        "step",
+                        ["zoom"],
+                        "",
+                        8,
+                        ["to-string", ["get", "vario"]]
+                    ]
+                },
+                "paint": {}
+            }
+        );
+        // End: Vario values
 
-    return "hsl(" + h + "," + s + "%," + l + "%)";
-}
+
+        filterBy(varioFilterValue, altInFilterValue);
+
+
+        $('#slider, #slider-altitude-in').on('input',
+            function (e) {
+                var $varioSlider = $('#slider')[0];
+                var $altitudeSlider = $('#slider-altitude-in')[0];
+
+                varioFilterValue = parseFloat($varioSlider.value, 2);
+                altInFilterValue = parseInt($altitudeSlider.value);
+                filterBy(varioFilterValue, altInFilterValue);
+            });
+    });
