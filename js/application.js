df5d80574b3f22ba5d68e00058ab08eaaf3ae5b7
Ludovic Launer <l.launer@idosens.com>
2020-02-19 15:19:57 +0100

    Add igc file upload

--- js/application.js
+++ js/application.js
@@ -61,3 +61,66 @@ function hexToHSL(H) {
 
     return "hsl(" + h + "," + s + "%," + l + "%)";
 }
+
+// isNetcoupeFlight
+// Returns: True if the flight is coming from a Netcoupe Id #
+function isNetcoupeFlight(flightId) {
+    var isNetcoupeFlight = !isNaN(flightId);
+    return isNetcoupeFlight;
+}
+
+// checkFlightCanBeAdded
+// Checks that the flight can be added onto the map
+// = True if it's not already processed
+function checkFlightCanBeAdded(flightId) {
+    // Check that the flight is not alrady there
+    if (trackIds.includes(flightId)) {
+        toastr["error"]("Le vol est déja présent !", "Vol Netcoupe #" + flightId);
+        return false;
+    } else {
+        trackIds.push(flightId);
+        $('#lbl-id').attr('title', trackIds.join(", "));    // Add to tooltip
+        return true;
+    }
+}
+
+// ---------- Setup and configure the drop zone for igc upload ----------
+function setupDropZone() {
+    var myDropzone = new Dropzone('#igc-upload', {
+        url: HeatmapRestAPIEndpoint + "/file/igc",
+        maxFiles: 1,
+        maxFilesize: 2,
+        acceptedFiles: ".igc"
+    });
+
+    myDropzone.on("success", function (file, response) {
+        myDropzone.removeAllFiles();
+        trackId = file.name;
+        loadNetcoupeFlight(trackId, response);
+    });
+
+    myDropzone.on("error", function (file, errorMessage, xhrResponse) {
+        myDropzone.removeAllFiles();
+        if (xhrResponse && xhrResponse.status === 500) {
+            toastr["error"]("Erreur pendant le traitement du fichier: " + file.name);
+        }
+        else if (xhrResponse && xhrResponse.status === 400) {
+            toastr["error"]("Fichier non valide: " + file.name, errorMessage.error);
+        }
+        else {
+            toastr["error"](errorMessage.error, "ok");
+        }
+    });
+
+    myDropzone.on("addedfile", function (e) {
+        //var flightId = e.dataTransfer.files[0].name;
+        var flightId = e.name;
+        if (!checkFlightCanBeAdded(flightId)) {
+            myDropzone.removeAllFiles();
+        }
+
+    });
+
+
+}
+
